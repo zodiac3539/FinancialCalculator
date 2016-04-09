79afe67c8847cd5f73823f6e07fe7551097a592c
zodiac3539 <zodiac3539@naver.com>
2016-04-09 15:23:48 -0400

    Finance Calculator Init
    Initial Codes

---
+++ menu.js
@@ -0,0 +1,5 @@
+document.write('| <a href=financecalculator.html>Finance Calculator</A> | ');
+document.write('<a href=justcalculator.html>Super Calculator</A> |<BR>');
+document.write('| <a href=zscore.html>Z Score</A> |');
+document.write('<BR>');
+    
